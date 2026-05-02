1748e84de8bf0de1c99a33b180ea6d628d3c86f2
yohanes091 <yohanesandhika09@gmail.com>
2026-05-02 22:20:15 +0700

    Create passport.js

---
+++ service/auth-service/config/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;

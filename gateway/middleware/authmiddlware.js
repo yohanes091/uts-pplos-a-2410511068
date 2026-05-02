561d9ca5331235cd008d4a9824a8de61013090ef
yohanes091 <yohanesandhika09@gmail.com>
2026-05-02 21:29:23 +0700

    Create authmiddlware.js

---
+++ gateway/middleware/authmiddlware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "Token required"
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(403).json({
+      success: false,
+      message: "Invalid token"
+    });
+  }
+};

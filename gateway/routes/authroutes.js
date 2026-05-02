a1fee97433e923eb5d25882d4828f9a7009f686b
yohanes091 <yohanesandhika09@gmail.com>
2026-05-02 21:55:24 +0700

    Create authroutes.js

---
+++ gateway/routes/authroutes.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
+
+module.exports = (app) => {
+  app.use(
+    "/auth",
+    createProxyMiddleware({
+      target: process.env.AUTH_SERVICE_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        "^/auth": ""
+      }
+    })
+  );
+};
